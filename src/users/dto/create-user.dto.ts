ad668c549f04c6eddc5d6f063c4bea20432d30bf
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-03-05 17:16:29 +0200

    Docs user resource & install class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsString } from "class-validator";
+import { Roles } from "src/utility/user.enum";
+
+export class CreateUserDto {
+
+    @IsString()
+    readonly username: string;
+
+    @IsString()
+    readonly email: string;
+
+    @IsString()
+    readonly password: string;
+
+    @IsString({ each: true })
+    readonly roles: Roles[];
+}
